e4e894db3b87295b144b8220571e604b623334c1
Minte Temple <mintetemple@gmail.com>
2023-02-07 03:58:55 -0800

    chore: fix prettier

---
+++ src/pages/components/index.tsx
@@ -0,0 +1,9 @@
+const ComponentPage = () => {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Component Page</h1>
+		</div>
+	);
+};
+
+export default ComponentPage;
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: [
+		"./pages/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}",
+		"./layouts/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		...defaultTheme,
+		extend: {
+			colors: {
+				"border-color": " #CBC4BC",
+				"primary-color": "var(--color-primary)",
+				"secondary-color": "var(--color-secondary)",
+				"tertiary-color": "var(--color-tertiary)",
+				"footer-color": "var(--color-footer)",
+				"header-border-color": "var(--color-header-border)",
+				"connector-tip-fill": "#4A5468",
+				ice: "#FFFFFF",
+				spruce: "#004F6C",
+				sea: "#05A1C6",
+				grass: "#50B848",
+				granite: "#3E3935",
+				"granite-dark": " #2C2E35",
+				sky: "#B3E5FC",
+				hemlock: "#046A38",
+				sunset: "#E8927C",
+				arbutus: "#FA4616",
+				stone: "#9C948D",
+				sand: "#CBC4BC",
+				"wf-base-600": "#717D96",
+				"wf-base-400": "#CBD2E0",
+				current: "currentColor",
+				"stone-light": "#9C948D1A",
+				...defaultTheme.colors,
+			},
+			backgroundColor: (theme) => ({
+				...theme("colors"),
+			}),
+		},
+	},
+	variants: {
+		backgroundColor: ["active"],
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
