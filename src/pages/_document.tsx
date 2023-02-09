8c8364dd02b89db8b5b2803910eabf898c514039
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-09 03:28:24 -0800

    Fix: fix rendering error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Countdown from './components/countdown';
 
 export default function Document() {
 	return (
@@ -7,7 +6,6 @@ export default function Document() {
 			<Head />
 			<body>
 				<Main />
-				<Countdown />
 				<NextScript />
 			</body>
 		</Html>
--- src/pages/components/countdown/index.tsx
+++ src/pages/components/countdown/index.tsx
@@ -1,49 +1,48 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 const Countdown = () => {
+	const [days, setDays] = useState(0);
+	const [hours, setHours] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	const [seconds, setSeconds] = useState(0);
 
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const countDownDate = new Date("Jul 1, 2023 00:00:00").getTime();
+			const now = new Date().getTime();
+			const difference = countDownDate - now;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const countDownDate = new Date("Jul 1, 2023 00:00:00").getTime();
-      const now = new Date().getTime();
-      const difference = countDownDate - now;
+			setDays(Math.floor(difference / (1000 * 60 * 60 * 24)));
+			setHours(
+				Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+			);
+			setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
+			setSeconds(Math.floor((difference % (1000 * 60)) / 1000));
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
 
-      setDays(Math.floor(difference / (1000 * 60 * 60 * 24)));
-      setHours(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
-      setSeconds(Math.floor((difference % (1000 * 60)) / 1000));
+	return (
+		<div className="container mx-auto">
+			<div>
+				<span className="time">{days}</span>
+				<span className="label">Days</span>
+			</div>
+			<div>
+				<span className="time">{hours}</span>
+				<span className="label">Hours</span>
+			</div>
+			<div>
+				<span className="time">{minutes}</span>
+				<span className="label">Minutes</span>
+			</div>
+			<div>
+				<span className="time">{seconds}</span>
+				<span className="label">Seconds</span>
+			</div>
+		</div>
+	);
+};
 
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-
-  return (
-    <div className='container mx-auto'>
-      <div>
-        <span className='time'>{days}</span>
-        <span className='label'>Days</span>
-      </div>
-      <div>
-        <span className='time'>{hours}</span>
-        <span className='label'>Hours</span>
-      </div>
-      <div>
-        <span className='time'>{minutes}</span>
-        <span className='label'>Minutes</span>
-      </div>
-      <div>
-        <span className='time'>{seconds}</span>
-        <span className='label'>Seconds</span>
-      </div>
-    </div>
-  )
-}
-
-export default Countdown
+export default Countdown;
