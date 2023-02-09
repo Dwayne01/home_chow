dd8b3171c76808c084546d4e4289155cd5171321
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-09 03:05:24 -0800

    Build: add logic to countdown by useEffect

--- src/pages/components/countdown/index.tsx
+++ src/pages/components/countdown/index.tsx
@@ -8,8 +8,41 @@ const Countdown = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const countDownDate = new Date("Jul 1, 2023 00:00:00").getTime();
+      const now = new Date().getTime();
+      const difference = countDownDate - now;
+
+      setDays(Math.floor(difference / (1000 * 60 * 60 * 24)));
+      setHours(Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      setMinutes(Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)));
+      setSeconds(Math.floor((difference % (1000 * 60)) / 1000));
+
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
-    <div>Countdown</div>
+    <div className='container mx-auto'>
+      <div>
+        <span className='time'>{days}</span>
+        <span className='label'>Days</span>
+      </div>
+      <div>
+        <span className='time'>{hours}</span>
+        <span className='label'>Hours</span>
+      </div>
+      <div>
+        <span className='time'>{minutes}</span>
+        <span className='label'>Minutes</span>
+      </div>
+      <div>
+        <span className='time'>{seconds}</span>
+        <span className='label'>Seconds</span>
+      </div>
+    </div>
   )
 }
 
