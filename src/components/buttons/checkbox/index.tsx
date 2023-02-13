ff3b4854c3315ba373130686e2e750a26f3da581
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-13 15:43:26 -0800

    Build: create checkbox component

---
+++ src/components/buttons/checkbox/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CheckBox = () =>
+    <>
+        <div className="container flex justify-center min-w-fit mx-auto p-10 m-7 border-2 rounded-full shadow">
+                <p className='content-center'>I am a Driver</p>
+                <input className="ml-4" type="checkbox" />
+        </div>
+        <div className="container flex justify-center min-w-fit mx-auto p-10 m-7 border-2 rounded-full shadow">
+                <p className='content-center'>I am a Lorem</p>
+                <input className="ml-4" type="checkbox" />
+        </div>
+        <div className="container flex justify-center min-w-fit mx-auto p-10 m-7 border-2 rounded-full shadow">
+                <p className='content-center'>I am a Lorem</p>
+                <input className="ml-4" type="checkbox" />
+        </div>
+    </>
+
+export default CheckBox
