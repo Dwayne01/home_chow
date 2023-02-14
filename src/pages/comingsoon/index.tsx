0f73f78ede858374994e1d086696e69f1b57de3c
Jaskaran Deogan <84167202+jaskarandeogan@users.noreply.github.com>
2023-02-14 13:47:38 -0800

    add coming soon layout

---
+++ src/pages/comingsoon/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import classNames from "classNames";
+import LayoutComponent from "../../components/countdown/layout";
+
+const ComingSoonPage = () => {
+	const LeftComponent = () => (
+		<div
+			className={classNames(
+				"w-full",
+				"flex flex-col justify-between",
+				"bg-slate-400"
+			)}
+		>
+			<span className="text-center">left side</span>
+			<div className="text-center">
+				<span>footer</span>
+			</div>
+		</div>
+	);
+
+	const RightComponent = () => (
+		<div
+			className={classNames(
+				"w-full bg-slate-600",
+				"flex flex-col justify-between"
+			)}
+		>
+			<span className="text-center">right side</span>
+		</div>
+	);
+
+	return (
+		<LayoutComponent className="">
+			<LeftComponent />
+			<RightComponent />
+		</LayoutComponent>
+	);
+};
+
+export default ComingSoonPage;
