89cb10d498574bfad93ba6d43f2ec286f596cebe
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-15 02:54:03 -0800

    Style: add style for media query

--- src/components/buttons/checkbox/index.tsx
+++ src/components/buttons/checkbox/index.tsx
@@ -15,20 +15,20 @@ const CheckBox = () => {
     }, [value])
 
     return (
-        <div className='sm:flex-column md:flex-row'>
-            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
+        <div className='flex flex-col sm:flex-row sm:gap-5 justify-center mt-5      '>
+            <button className="container flex justify-center min-w-fit p-5 m-3 border-2 rounded-full shadow text-xl text-stone-500 hover:bg-yellow-50 hover:border-yellow-500 hover:text-yellow-500">
                 <label htmlFor='checkbox-1' className=''>I am a Driver
                     <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-1' type="checkbox" value="driver" onChange={handleChange} />
                 </label>
             </button>
 
-            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
+            <button className="container flex justify-center min-w-fit p-5 m-3 border-2 rounded-full shadow text-xl text-stone-500 hover:bg-yellow-50 hover:border-yellow-500 hover:text-yellow-500">
                 <label htmlFor='checkbox-2'>I am a Chef
                     <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-2' type="checkbox" value="chef" onChange={handleChange} />
                 </label>
             </button>
 
-            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
+            <button className="container flex justify-center min-w-fit p-5 m-3 border-2 rounded-full shadow text-xl text-stone-500 hover:bg-yellow-50 hover:border-yellow-500 hover:text-yellow-500">
                 <label htmlFor='checkbox-3'>I am a Customer
                     <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-3' type="checkbox" value="customer" onChange={handleChange} />
                 </label>
