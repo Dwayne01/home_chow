9c10618cecda73c39e91bde552b53a7c14755956
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-15 02:21:22 -0800

    Chore: set screen size to tailwind config

--- src/components/buttons/checkbox/index.tsx
+++ src/components/buttons/checkbox/index.tsx
@@ -7,32 +7,32 @@ const CheckBox = () => {
         e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
       ) => {
         setValue(e.target.value);
-      };
-
+    };
+ 
     useEffect(() => {
         // eslint-disable-next-line no-console
         console.log(value)
     }, [value])
 
     return (
-        <>
-            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500" >
-                <label htmlFor='checkbox-1'>I am a Driver
+        <div className='sm:flex-column md:flex-row'>
+            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
+                <label htmlFor='checkbox-1' className=''>I am a Driver
                     <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-1' type="checkbox" value="driver" onChange={handleChange} />
                 </label>
             </button>
 
             <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
                 <label htmlFor='checkbox-2'>I am a Chef
-                    <input className="checkbox ml-4 mt-2 accent-yellow-500" id='input-2' type="checkbox" value="chef" onChange={handleChange} />
+                    <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-2' type="checkbox" value="chef" onChange={handleChange} />
                 </label>
             </button>
 
-            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500" onClick={handleChange}>
+            <button className="container flex justify-center min-w-fit mx-auto p-8 m-7 border-2 rounded-full shadow text-xl text-stone-500 focus:bg-yellow-50 focus:border-yellow-500 focus:text-yellow-500">
                 <label htmlFor='checkbox-3'>I am a Customer
-                    <input className="checkbox ml-4 mt-2 accent-yellow-500" id='input-3' type="checkbox" value="customer" />
+                    <input className="checkbox ml-4 mt-2 accent-yellow-500" id='checkbox-3' type="checkbox" value="customer" onChange={handleChange} />
                 </label>
             </button>
-        </>
+        </div>
 )}
 export default CheckBox
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
 	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		screens: {
+			"2xl": "1536px",
+			xl: "1280px",
+			lg: "1024px",
+			md: "768px",
+			sm: "640px",
+			xs: "480px",
+		},
 	},
 	plugins: [],
 };
