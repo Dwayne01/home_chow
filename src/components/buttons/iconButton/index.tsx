aa3b9e3072334ceba6c7c03a7c39798fdc66710a
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-15 19:31:21 -0800

    Update: install react-icons & styling

--- src/components/buttons/iconButton/index.tsx
+++ src/components/buttons/iconButton/index.tsx
@@ -1,13 +1,19 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
-
-const IconButton: React.FC<ButtonProps> = ({ children, ...rest }) => (
-    <button
-    {...rest}
-    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50">
-    {children}
-    </button>
+const IconButton = () => (
+    
+    <div className='flex gap-7 m-10'>
+        <button className="px-4 py-2 bg-gray-100 text-black rounded focus:outline-none focus:ring-2 focus:ring-[#EAAA08] focus:ring-opacity-80">
+            <FaFacebookF color="#EAAA08" fontSize="1.5em" />
+        </button>
+        <button className="px-4 py-2 bg-gray-100 text-black rounded focus:outline-none focus:ring-2 focus:ring-[#EAAA08] focus:ring-opacity-80">
+            <FaTwitter color="#EAAA08" fontSize="1.5em" />
+        </button>
+        <button className="px-4 py-2 bg-gray-100 text-black rounded focus:outline-none focus:ring-2 focus:ring-[#EAAA08] focus:ring-opacity-80">
+            <FaLinkedinIn color="#EAAA08" fontSize="1.5em" />
+        </button>
+    </div>
+    
 );
 
 export default IconButton;
