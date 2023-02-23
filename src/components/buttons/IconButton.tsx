30813a52083f95235b3d549edbf88a10ca06230b
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-02-22 19:50:32 -0800

    Update

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -12,20 +12,13 @@ const IconButton = ({
 	href: string;
 	color: string;
 	bgColor: string;
-}) => {
-	const colorVariants = {
-		button: `bg-[${bgColor}] rounded-[10px] w-[60px] h-[60px] focus:outline-none focus:ring-2 focus:ring-[${color}] focus:ring-opacity-80`,
-		text: `flex justify-center text-2xl text-[${color}]`,
-	};
-	return (
-		<Link href={href}>
-			<button className={`${colorVariants.button}`}>
-				<div className={`${colorVariants.text}`}>
-					{React.createElement(icon)}
-				</div>
-			</button>
-		</Link>
-	);
-};
+}) => (
+	<Link
+		href={href}
+		className={`bg-[${bgColor}] rounded-[10px] w-[60px] h-[60px] focus:outline-none focus:ring-2 focus:ring-[${color}] focus:ring-opacity-80 flex justify-center text-2xl items-center`}
+	>
+		{React.createElement(icon, { color })}
+	</Link>
+);
 
 export default IconButton;
--- src/components/buttons/IconButtonGroup.tsx
+++ src/components/buttons/IconButtonGroup.tsx
@@ -2,7 +2,7 @@ import IconButton from "@/components/buttons/IconButton";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
 const IconButtonGroup = () => (
-	<div className="flex gap-5">
+	<div className="flex justify-center gap-5">
 		<IconButton icon={FaFacebookF} color="#EAAA08" bgColor="#FCFCFD" href="" />
 		<IconButton icon={FaTwitter} color="#EAAA08" bgColor="#FCFCFD" href="" />
 		<IconButton icon={FaLinkedinIn} color="#EAAA08" bgColor="#FCFCFD" href="" />
