516c795f9286ebaa9f634ff64af6d7a642039628
Minte Temple <mintetemple@gmail.com>
2023-02-25 01:32:34 -0800

    chore: refactored button

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,14 +1,43 @@
 import Countdown from "@/components/countdown";
-import InputButtonGroup from "@/components/buttons/InputButtonGroup";
+import CheckboxButton from "@/components/buttons/CheckboxButton";
+import { useState } from "react";
 
-const ComponentPage = () => (
-	<div>
-		<h1 className="text-3xl text-red-600 font-bold underline">
-			Components Page
-		</h1>
-		<Countdown />
-		<InputButtonGroup />
-	</div>
-);
+const ComponentPage = () => {
+	const [selectedValue, setSelectedValue] = useState<string>("");
+
+	return (
+		<div>
+			<h1 className="text-3xl text-red-600 font-bold underline">
+				Components Page
+			</h1>
+			<Countdown />
+			<div className="flex flex-col sm:flex-row justify-center sm:mx-10 mx-10 gap-5">
+				<CheckboxButton
+					name="customer"
+					label="I am a Customer"
+					value={selectedValue}
+					isChecked={selectedValue === "customer"}
+					handleClick={(data) => {
+						setSelectedValue(data);
+					}}
+				/>
+				<CheckboxButton
+					name="vendor"
+					label="I am a Vendor"
+					value={selectedValue}
+					isChecked={selectedValue === "vendor"}
+					handleClick={() => setSelectedValue("vendor")}
+				/>
+				<CheckboxButton
+					name="driver"
+					label="I am a Driver"
+					value={selectedValue}
+					isChecked={selectedValue === "driver"}
+					handleClick={() => setSelectedValue("driver")}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default ComponentPage;
