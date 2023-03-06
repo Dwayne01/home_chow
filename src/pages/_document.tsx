5f6d1ae067e9cf3f6236c6f032717306338d3a66
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-03-06 00:36:37 -0800

    Chore: add homechow favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<link
+					rel="shortcut icon"
+					href="/assets/images/logo/HomeChow_Favicon.png"
+					type="image/x-icon"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
