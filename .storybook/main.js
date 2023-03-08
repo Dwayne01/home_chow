d245ebf2d89bb8e798d6f632a65d65e19524e80a
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-03-08 13:03:22 -0800

    Update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,18 @@ module.exports = {
 	addons: [
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
+		"@storybook/addon-controls",
 		"@storybook/addon-interactions",
 	],
 	framework: "@storybook/react",
 	core: {
 		builder: "@storybook/builder-webpack5",
+	},
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+		});
+		return config;
 	},
 };
--- src/stories/WideIconButton.stories.tsx
+++ src/stories/WideIconButton.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { FaFacebook } from "react-icons/fa";
+import { FaGoogle, FaFacebook, FaTwitter } from "react-icons/fa";
 import WideIconButton from "../components/common/buttons/WideIconButton";
 
 export default {
@@ -14,6 +14,32 @@ const Template: ComponentStory<typeof WideIconButton> = (args) => (
 
 export const SignInGoogle = Template.bind({});
 SignInGoogle.args = {
+	icon: FaGoogle,
+	label: "Sign in with Google",
+};
+
+export const SignInFacebook = Template.bind({});
+SignInFacebook.args = {
 	icon: FaFacebook,
 	label: "Sign in with Google",
+};
+
+export const SignInTwitter = Template.bind({});
+SignInTwitter.args = {
+	icon: FaTwitter,
+	label: "Sign in with Google",
+};
+
+export const SignIn = Template.bind({});
+SignIn.args = {
+	label: "Sign in",
+	textColor: "text-white",
+	bgColor: "bg-primary-color",
+};
+
+export const GetStarted = Template.bind({});
+SignIn.args = {
+	label: "Get Started",
+	textColor: "text-white",
+	bgColor: "bg-primary-color",
 };
