92870c35a4a5e3274d34bf891d025e3fc80b3e0e
Jaskaran Deogan <84167202+jaskarandeogan@users.noreply.github.com>
2023-03-13 15:42:12 -0700

    internationalization

--- src/components/VendorDashboard.tsx
+++ src/components/VendorDashboard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StatisticsList from "@/components/common/Statistics/StatisticsList";
+import { useTranslation } from "next-i18next";
 import People from "../../public/assets/svg/People";
 import Bag from "../../public/assets/svg/Bag";
 import Carbon from "../../public/assets/svg/Carbon";
@@ -29,22 +30,25 @@ const statistics = [
 ];
 
 const VendorDashboard = ({ vendorName = "Django" }) => {
+	const { t } = useTranslation("dashboard");
+
 	const daylight = new Date().getHours() < 12 ? "Morning" : "Afternoon";
 	const daylightSticker = new Date().getHours() < 12 ? "🌤️" : "🌙";
 	const greeting = `Good ${daylight}`;
+
 	return (
 		<div className="max-w-5xl flex flex-col mx-[86px] xl:m-auto justify-center pt-[42px]">
 			<div>
 				<h4 className="text-center font-semibold text-xl">
-					{greeting}, {vendorName} {daylightSticker}
+					{t(greeting)}, {vendorName} {daylightSticker}
 				</h4>
 				<p className="text-gray-40 mt-3 text-center font-normal text-base">
-					Take a look at your store today
+					{t("Take a look at your store today")}
 				</p>
 			</div>
 			<div className="mt-8">
 				<h4 className="text-base text-gray-40 leading-6 uppercase">
-					Statistics
+					{t("Statistics")}
 				</h4>
 				<div className="mt-4">
 					<StatisticsList {...{ statistics }} />
--- src/components/common/Statistics/StatisticsCard.tsx
+++ src/components/common/Statistics/StatisticsCard.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { useTranslation } from "next-i18next";
 import { formateLargeNumber } from "@/utils";
 import { StatisticsCardProps } from "@/types/dashboard";
 
-const StatisticsCard = ({ Icon, count, label }: StatisticsCardProps) => (
-	<div className="flex p-4 gap-4 border border-border-neutral rounded-lg w-[200px] hover:bg-white hover:shadow cursor-default">
-		<div>{Icon}</div>
-		<div className="flex flex-col">
-			<span className="font-semibold text-2xl">
-				{formateLargeNumber(count)}
-			</span>
-			<span>{label}</span>
+const StatisticsCard = ({ Icon, count, label }: StatisticsCardProps) => {
+	const { t } = useTranslation("dashboard");
+
+	return (
+		<div className="flex p-4 gap-4 border border-border-neutral rounded-lg w-[200px] bg-white hover:shadow cursor-default overflow-hidden">
+			<div>{Icon}</div>
+			<div className="flex flex-col">
+				<span className="font-semibold text-2xl">
+					{formateLargeNumber(count)}
+				</span>
+				<span className="whitespace-nowrap ">{t(label)}</span>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default StatisticsCard;
