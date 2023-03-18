ae6f788078c601534e4b318e9b80c0bf6516c849
Minte Temple <mintetemple@gmail.com>
2023-03-18 15:09:31 -0700

    chore: update

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
 	e2e: {
+		supportFile: false,
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		setupNodeEvents(_on, _config) {
 			// implement node event listeners here
