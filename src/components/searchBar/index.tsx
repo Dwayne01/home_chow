a85e5153e68024cc1d7fcc7cb9fe2436179f7ab3
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-06 23:06:42 -0700

    Build: create modal for address search

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -16,6 +16,7 @@ const SearchBar = ({
 	rootClass?: string;
 }) => {
 	const [query, setQuery] = useState<string>("");
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setQuery(e.target.value);
@@ -27,31 +28,55 @@ const SearchBar = ({
 	};
 
 	return (
-		<form
-			onSubmit={handleSubmit}
-			className={classNames(
-				"flex items-center border rounded-full px-3 py-2",
-				width || "w-[329px] md:w-[567px]",
-				rootClass || ""
-			)}
-		>
-			<input
-				type="text"
-				placeholder={placeholder}
-				value={query}
-				onChange={handleInputChange}
-				className="w-full text-gray-700 bg-transparent border-none focus:outline-none"
-			/>
-			<button
-				type="submit"
+		<div>
+			<form
+				onSubmit={handleSubmit}
 				className={classNames(
-					"ml-3 p-2 rounded-full",
-					iconBgColor || "bg-primary-color"
+					"flex justify-center items-center border rounded-full shadow-sm px-6 py-3",
+					width || "w-[329px] md:w-[567px]",
+					rootClass || ""
 				)}
 			>
-				<BiSearch className="w-5 h-5 text-white" />
-			</button>
-		</form>
+				<div className="w-11/12">
+					<div>
+						<h6>Where</h6>
+					</div>
+					<input
+						type="text"
+						placeholder={placeholder}
+						value={query}
+						onChange={handleInputChange}
+						onFocus={() => setIsModalOpen(true)}
+						className="w-full text-gray-700 bg-transparent border-none focus:outline-none mt-1"
+					/>
+				</div>
+
+				<button
+					type="submit"
+					className={classNames(
+						"ml-3 p-2 rounded-full w-[48px] h-[48px]",
+						iconBgColor || "bg-primary-color"
+					)}
+				>
+					<BiSearch className="w-[24px] h-[24px] text-white m-auto" />
+				</button>
+			</form>
+			{isModalOpen && (
+				<div
+					className={classNames(
+						"shadow-md rounded-lg px-6",
+						width || "w-[329px] md:w-[567px]",
+						rootClass || ""
+					)}
+				>
+					<input
+						type="text"
+						placeholder="Search location..."
+						className="border rounded-lg px-3 py-2 w-full my-5 ring-green-500"
+					/>
+				</div>
+			)}
+		</div>
 	);
 };
 
