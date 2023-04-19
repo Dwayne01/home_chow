9a52a0544405f92074be5e49c29fe7bff860c339
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-19 10:51:07 -0700

    Update: fix cypress img rendeing issue & add more testing script

--- src/components/accordion/Accordion.cy.tsx
+++ src/components/accordion/Accordion.cy.tsx
@@ -1,30 +1,42 @@
+import { applyTheme } from "@/theme/utils";
 import Accordion from "./index";
 import foodImage from "../../../public/assets/images/food.jpg";
+import "../../styles/globals.css";
 
-describe("Accordion", () => {
-	const items = [
-		{
-			title: "White Stew",
-			price: 10.95222,
-			content:
-				"Wings tossed in your choice of sauce, served with carrots, celery sticks, and dipping sauce. Choose at least one.",
-			cardList: ["Mild Sauce", "Hot Sauce", "Medium Sauce"],
-			image: foodImage,
-		},
-	];
+const items = [
+	{
+		title: "White Stew",
+		price: 10.95222,
+		content:
+			"Wings tossed in your choice of sauce, served with carrots, celery sticks, and dipping sauce. Choose at least one.",
+		cardList: ["Mild Sauce", "Hot Sauce", "Medium Sauce"],
+		image: foodImage,
+	},
+];
+
+applyTheme();
+describe("<Accordion />", () => {
+	beforeEach(() => {
+		// Load a local image and intercept requests for Next.js image components
+		cy.readFile("public/assets/images/food.jpg", null).then((img) => {
+			cy.intercept("_next/image*", {
+				statusCode: 200,
+				headers: { "Content-Type": "image/png" },
+				body: img.buffer,
+			});
+			cy.mount(<Accordion items={items} />);
+		});
+	});
 
 	it("renders with the correct props", () => {
-		// render accordion
-		cy.mount(<Accordion items={items} />);
-
-		// check proper props are rendered
+		// Check proper props are rendered
 		cy.get(".accordion-title").should("have.text", items[0].title);
 		cy.get(".accordion-price").should("have.text", `$${items[0].price}`);
 
-		// click to expand accordion
+		// Click to expand accordion
 		cy.get(".accordion-arrow").click();
 
-		// check proper props are rendered
+		// Check proper props are rendered
 		cy.get(".accordion-content").should("have.text", items[0].content);
 		cy.get(".accordion-cardList").should(
 			"have.text",
@@ -33,10 +45,7 @@ describe("Accordion", () => {
 	});
 
 	it("tests the user flow for placing an order", () => {
-		// render accordion
-		cy.mount(<Accordion items={items} />);
-
-		// click to expand accordion
+		// Click to expand accordion
 		cy.get(".accordion-arrow").click();
 
 		// Select the option from the cardList
