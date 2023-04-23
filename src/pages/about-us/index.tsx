132e3b70cc2fe9117fecc4778d8199b500c206bd
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-23 01:14:05 -0700

    Chore: add footer comp & fix style issue

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -2,6 +2,7 @@ import ComingSoonHeader from "@/components/header/ComingSoonHeader";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Image from "next/image";
+import Footer from "@/components/footer/Footer";
 import foodPotImage from "../../../public/assets/images/food_pot.png";
 import foodBowlImage from "../../../public/assets/images/food_bowl.png";
 // import glassJarImage from "../../../public/assets/images/jar.png";
@@ -15,7 +16,7 @@ const AboutUs = () => {
 			<div className="mt-2">
 				<ComingSoonHeader />
 				<div className="m-auto mt-7 w-[95%]">
-					<div className="pl-1">
+					<div className="pl-2">
 						<h1 className="mb-2">{t("About us")}</h1>
 						<h2 className="mb-2 w-[70%] font-medium">
 							{t("Bringing homecooked meals to your")}
@@ -26,7 +27,7 @@ const AboutUs = () => {
 						</h2>
 					</div>
 					<div className="grid grid-cols-2 grid-rows-4 gap-4 p-1">
-						<div className="mt-6 pl-1 pr-6 py-3">
+						<div className="mt-2 pl-1 pr-6 pt-5">
 							<p className="text-gray-paragraph leading-7">
 								{t("With the increasing demand")}
 							</p>
@@ -37,14 +38,14 @@ const AboutUs = () => {
 						<div>
 							<Image src={foodPotImage} alt="homemade pot stew" className="" />
 						</div>
-						<div className="pl-1 pr-2 py-3">
+						<div className="pl-1 pr-2 pt-2">
 							<p className="text-gray-paragraph leading-7">
 								{t("To cater to the diverse residents of Vancouver")}
 							</p>
 						</div>
-						<div className="pl-1 pr-2 py-3">
+						<div className="pl-1 pr-2 pt-3">
 							<p className="text-gray-paragraph leading-7">
-								<strong className="text-black">{t("HomeChow")}</strong>{" "}
+								<strong>{t("HomeChow")}</strong>{" "}
 								{t("is a food delivery service that offers")}
 							</p>
 						</div>
@@ -60,13 +61,14 @@ const AboutUs = () => {
 								className="relative"
 							/>
 						</div>
-						<div className="col-span-2 pl-1 pr-2 py-3">
+						<div className="col-span-2 pl-1 pr-2 mt-[-10%]">
 							<p className="text-gray-paragraph leading-7">
 								{t("from classic comfort food to international cuisine")}
 							</p>
 						</div>
 					</div>
 				</div>
+				<Footer />
 			</div>
 		</div>
 	);
