c460d83663450b618e6e2c27ab8a3f7701ffa64e
Blessed Dominic <blesseddominic98@gmail.com>
2023-06-20 15:05:41 +0100

    refactored the tab.cy test

--- src/components/common/tab/tab.cy.tsx
+++ src/components/common/tab/tab.cy.tsx
@@ -1,35 +1,34 @@
-//  import React from "react";
-// import { mount } from "@cypress/react";
-// import Tabs, { TabProps } from "@/components/common/tab";
-// import "../../../styles/globals.css";
+import React from "react";
+import { mount } from "@cypress/react";
+import Tabs, { TabProps } from "@/components/common/tab";
+import "../../../styles/globals.css";
 
-// describe("Tab component", () => {
-// 	const tabs: TabProps[] = [
-// 		{
-// 			label: "Tab 1 Title",
-// 			children: <div>Content for tab 1</div>,
-// 			onClick: () => {},
-// 		},
-// 		{
-// 			label: "Tab 2 Title",
-// 			children: <div>Content for tab 2</div>,
-// 			onClick: () => {},
-// 		},
-// 		{
-// 			label: "Tab 3 Title",
-// 			children: <div>Content for tab 3</div>,
-// 			onClick: () => {},
-// 		},
-// 	];
+describe("Tab component", () => {
+	const tabs: TabProps[] = [
+		{
+			label: "Tab 1 Title",
+			children: <div>Content for tab 1</div>,
+			// onClick: () => {},
+		},
+		{
+			label: "Tab 2 Title",
+			children: <div>Content for tab 2</div>,
+			// onClick: () => {},
+		},
+		{
+			label: "Tab 3 Title",
+			children: <div>Content for tab 3</div>,
+			// onClick: () => {},
+		},
+	];
 
-// 	it("should display active tab content", () => {
-// 		mount(<Tabs tabs={tabs} />);
-// 		cy.contains("Tab 1 Title").click();
-// 		cy.get('[data-cy="tab"]').eq(0).should("contain", "Content for tab 1");
-// 		cy.contains("Tab 2 Title").click();
-// 		cy.get('[data-cy="tab"]').eq(1).should("contain", "Content for tab 2");
-// 		cy.contains("Tab 3 Title").click();
-// 		cy.get('[data-cy="tab"]').eq(2).should("contain", "Content for tab 3");
-// 	});
-// });
-export {};
+	it("should display active tab content", () => {
+		mount(<Tabs tabs={tabs} />);
+		cy.contains("Tab 1 Title");
+		cy.get('[data-cy="tab"]').eq(0).should("contain", "Content for tab 1");
+		cy.contains("Tab 2 Title");
+		cy.get('[data-cy="tab"]').eq(1).should("contain", "Content for tab 2");
+		cy.contains("Tab 3 Title");
+		cy.get('[data-cy="tab"]').eq(2).should("contain", "Content for tab 3");
+	});
+});
